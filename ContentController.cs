3b00df6f26437a70f4826ae2bd65ce553ead26db
Jonas Eriksson <jonas@programenta.se>
2013-09-20 12:22:33 +0200

    Initial commit

---
+++ ContentController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Web.Mvc;
+using Umbraco.Web.WebApi;
+
+namespace OurPublishedContentApi
+{
+    [PluginController("PublishedContent")]
+    public class NodeController : UmbracoApiController
+    {
+        public object GetNode(int? id=0)
+        {            
+            var node = umbraco.uQuery.GetNode(id??0);
+            return ContentHelpers.NodeProperties(node);
+        }
+        public object GetNode(string path)
+        {
+            var node = umbraco.uQuery.GetNodeByUrl(path);
+            return ContentHelpers.NodeProperties(node);
+        }
+        public object GetChildren(int? id = 0)
+        {
+            var node = umbraco.uQuery.GetNode(id ?? 0);
+            return ContentHelpers.Children(node);
+        }
+        public object GetChildren(string path)
+        {
+            var node = umbraco.uQuery.GetNodeByUrl(path);
+            return ContentHelpers.Children(node);
+        }
+        public object GetAncestors(int? id = 0)
+        {
+            var node = umbraco.uQuery.GetNode(id ?? 0);
+            return ContentHelpers.Ancestors(node);
+        }
+        public object GetAncestors(string path)
+        {
+            var node = umbraco.uQuery.GetNodeByUrl(path);
+            return ContentHelpers.Ancestors(node);
+        }
+        public object GetParent(int? id = 0)
+        {
+            var node = umbraco.uQuery.GetNode(id ?? 0);
+            return ContentHelpers.Parent(node);
+        }
+        public object GetParent(string path)
+        {
+            var node = umbraco.uQuery.GetNodeByUrl(path);
+            return ContentHelpers.Parent(node);
+        }
+    }
+
+
+}
---
+++ ContentServer.cs
@@ -0,0 +1,49 @@
+﻿using umbraco.NodeFactory;
+using System.Linq;
+using umbraco;
+
+namespace OurPublishedContentApi
+{
+    public class ContentHelpers
+    {
+        public static object Children(Node node)
+        {
+            return node.ChildrenAsList.Select(n => NodeProperties(n as Node));
+        }
+        public static object Parent(Node node)
+        {
+            return NodeProperties(node.Parent as Node);
+        }
+        public static object Ancestors(Node node)
+        {
+            return node.GetAncestorNodes().Select(n => NodeProperties(n));
+        }
+        public static object Descendants(Node node)
+        {
+            return node.GetDescendantNodes().Select(n => NodeProperties(n));
+        }
+        public static object NodeProperties(Node node)
+        {
+            return
+            new
+            {
+                node.Id,
+                node.Level,
+                node.NiceUrl,
+                node.Name,
+                node.UrlName,
+                node.NodeTypeAlias,
+                node.CreatorName,
+                node.template,
+                /*Template = template.Alias,*/
+                Properties = node.PropertiesAsList.Select(p => new { p.Alias, Value = p.Value }).ToDictionary(k => k.Alias, k => k.Value),
+                node.CreateDate,
+                node.UpdateDate,
+                node.SortOrder,
+                node.Url,
+                ParentId = (node.Parent != null) ? node.Parent.Id : -1,
+                ChildIds = node.ChildrenAsList.Select(n => n.Id)
+            };
+        }
+    }
+}
